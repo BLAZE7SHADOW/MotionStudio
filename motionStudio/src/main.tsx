c92f193db41a554becde5bd2091112c5063d73c3
Shivam Govind Rao <shivamgrao9161@gmail.com>
2026-07-15 23:14:01 +0530

    add PostHog analytics via PostHogProvider

--- motionStudio/src/main.tsx
+++ motionStudio/src/main.tsx
@@ -1,10 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { PostHogProvider } from 'posthog-js/react'
 import './index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <PostHogProvider
+      apiKey={import.meta.env.VITE_POSTHOG_KEY}
+      options={{
+        api_host: import.meta.env.VITE_POSTHOG_HOST,
+        defaults: '2026-05-30',
+      }}
+    >
+      <App />
+    </PostHogProvider>
   </StrictMode>,
 )
